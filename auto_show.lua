a9fb4af8a9e9c951020ce4c229c5c59910c4c120
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-10-13 19:26:39 +0200

    Add ability to automatically toggle mixer dock state

--- auto_show.lua
+++ auto_show.lua
@@ -25,5 +25,26 @@ function getKeyPos(window_height)
    return trigger_height, mixer_height, vertical_correction
 end
 
--- for tests only
--- if reaper.time_precise() - startclocktime >= script_duration then return end
+function updateMixer()
+   x, y = reaper.GetMousePosition()
+   state = reaper.GetToggleCommandState(40078)
+   
+   window_height = GetClientBounds(reaper.GetMainHwnd()) -- full = v(70, 1076)
+   trigger_height, mixer_height, vertical_correction = getKeyPos(window_height)
+   
+   if state == 0 and y > trigger_height and window_height > 500 then
+      reaper.Main_OnCommand(40078, 0)
+      reaper.JS_Mouse_SetPosition(x, y - vertical_correction)
+   elseif state == 1 and y <= mixer_height then
+      reaper.Main_OnCommand(40078, 0)
+   end
+
+   -- for tests only
+   -- if reaper.time_precise() - startclocktime >= script_duration then return end
+   
+   reaper.defer(updateMixer)
+end
+
+SetCommandState(1)
+updateMixer()
+reaper.atexit(SetCommandState)
