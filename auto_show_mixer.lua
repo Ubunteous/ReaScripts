258389d0d3e19484f87c82c230947382f0eb7e2d
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-10-14 19:41:19 +0200

    Made every variable local

--- auto_show_mixer.lua
+++ auto_show_mixer.lua
@@ -33,22 +33,22 @@ function sleep(n)
 end
 
 function setCommandState(set)
-   is_new_value, filename, sec, cmd, mode, resolution, val = reaper.get_action_context()
+   local is_new_value, filename, sec, cmd, mode, resolution, val = reaper.get_action_context()
    reaper.SetToggleCommandState(sec, cmd, set or 0)
    -- reaper.RefreshToolbar2(sec, cmd)
 end
 
 function getClientBounds(hwnd)
-   ret, left, top, right, bottom = reaper.JS_Window_GetClientRect(hwnd)
+   local ret, left, top, right, bottom = reaper.JS_Window_GetClientRect(hwnd)
    return bottom-top -- , top, bottom
 end
 
 function getMixerHeight()
    local hWnd_array = reaper.new_array({}, 100)
    reaper.JS_Window_ArrayFind("REAPER_dock", true, hWnd_array) -- side effect on hWnd_array
-   dock = hWnd_array.table()
+   local dock = hWnd_array.table()
    
-   if (#dock) == 0 then
+   if #dock == 0 then
       return 0
    end
 
@@ -59,27 +59,27 @@ function getMixerHeight()
 end
 
 function getMonitorHeight()
-   _, _, _, monitor_bottom = reaper.JS_Window_MonitorFromRect(0, 0, 0, 0, false)
+   local _, _, _, monitor_bottom = reaper.JS_Window_MonitorFromRect(0, 0, 0, 0, false)
    return monitor_bottom -- 1080
 end
 
 function getKeyPos(window_height, mixer_height)
    -- corner case: use math.min() if the window is not fullscreen. I am maybe too cautious
-   trigger_open_height = math.min(math.floor(mixer_height * 1.75) + window_height * .05, window_height)
-   trigger_close_height = math.floor(mixer_height - window_height * .075)
-   vertical_correction = math.floor((window_height - mixer_height) / 2)
+   local trigger_open_height = math.min(math.floor(mixer_height * 1.75) + window_height * .05, window_height)
+   local trigger_close_height = math.floor(mixer_height - window_height * .075)
+   local vertical_correction = math.floor((window_height - mixer_height) / 2)
    return trigger_open_height, trigger_close_height, vertical_correction
 end
 
 function isMainWindowFocused()
    -- verification necessary so that we do not get docks popping up when we are browsing menus
-   HWND_WindowFocus = reaper.JS_Window_GetFocus()
-   parent_WindowFocus = reaper.JS_Window_GetParent(HWND_WindowFocus)
-   match = ".*REAPER.*" -- perhaps ".*REAPER.*)" would be better
+   local HWND_WindowFocus = reaper.JS_Window_GetFocus()
+   local parent_WindowFocus = reaper.JS_Window_GetParent(HWND_WindowFocus)
+   local match = ".*REAPER.*" -- perhaps ".*REAPER.*)" would be better
 
    for _, window in ipairs({ HWND_WindowFocus, parent_WindowFocus }) do
       if window then
-	 window_name = reaper.JS_Window_GetTitle(window)
+	 local window_name = reaper.JS_Window_GetTitle(window)
 	 -- reaper.ShowConsoleMsg("\n Window: " .. tostring(window_name) .. "(" .. tostring(window) .. ") ")
 
 	 if string.match(window_name, match) then
@@ -104,8 +104,8 @@ function updateMixer()
       return nil
    end
 
-   mixer_height = getMixerHeight()
-   mixer_state = reaper.GetToggleCommandState(mixer_command_id)
+   local mixer_height = getMixerHeight()
+   local mixer_state = reaper.GetToggleCommandState(mixer_command_id)
 
    if mixer_height < monitor_height / 4 then
       -- the mixer is useless outside fullscreen. hide it
@@ -118,9 +118,9 @@ function updateMixer()
       return nil
    end
    
-   mouse_x, mouse_y = reaper.GetMousePosition()
-   window_height = getClientBounds(reaper.GetMainHwnd()) -- full = v(70, 1076)
-   trigger_open_height, trigger_close_height, vertical_correction = getKeyPos(window_height, mixer_height)
+   local mouse_x, mouse_y = reaper.GetMousePosition()
+   local window_height = getClientBounds(reaper.GetMainHwnd()) -- full = v(70, 1076)
+   local trigger_open_height, trigger_close_height, vertical_correction = getKeyPos(window_height, mixer_height)
 
    if mixer_state == CLOSED and mouse_y > trigger_open_height then
       reaper.Main_OnCommand(mixer_command_id, 0)
@@ -137,8 +137,10 @@ end
 
 function main()
    -- hack: force the dock to appear once as JS_Window_ArrayFind fails otherwise on app startup
-   reaper.Main_OnCommand(mixer_command_id, 0)
-   reaper.Main_OnCommand(mixer_command_id, 0)
+   if reaper.GetToggleCommandState(mixer_command_id) == CLOSED then
+      reaper.Main_OnCommand(mixer_command_id, 0)
+      reaper.Main_OnCommand(mixer_command_id, 0)
+   end
    
    setCommandState(1)
    monitor_height = getMonitorHeight() -- if I make it local, I will need to poll it frequently
