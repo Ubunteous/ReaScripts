65408bf88845a29887623335c89e665862a6633f
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-10-24 11:59:08 +0200

    Changed behaviour: insert multi FX + find recent track

--- Instantiate/Instantiate FX.lua
+++ Instantiate/Instantiate FX.lua
@@ -1,14 +1,22 @@
 function instantiateFX(FX)
-   -- Insert a plugin in the last track selected
-   -- Create a new track if necessary
-   
-   -- get the last touched track
-   -- track = reaper.GetLastTouchedTrack()
+   -- Insert a plugin in the last track selected (or a new Onega)
 
-   -- get the last track among currently selected
    nb_selected_tracks = reaper.CountSelectedTracks(0)
-   track = reaper.GetSelectedTrack(0, nb_selected_tracks - 1)
 
+   if nb_selected_tracks > 0 then
+      -- only insert on last track among selected
+      -- track = reaper.GetSelectedTrack(0, nb_selected_tracks - 1)
+
+      -- insert on every track selected
+      for nth_track = 0, nb_selected_tracks - 1 do
+	 track = reaper.GetSelectedTrack(0, nth_track)
+	 reaper.TrackFX_AddByName(track, FX, false, -1)
+      end
+      return 0
+   else
+      track = reaper.GetLastTouchedTrack()
+   end
+   
    if not track then
       nb_tracks = reaper.CountTracks(0)
       reaper.InsertTrackAtIndex(nb_tracks, false)
