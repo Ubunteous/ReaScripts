b9c0d84628bb6597aa8200584c5cf2a522cace8f
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2024-10-24 14:57:04 +0200

    Color folder tracks after insertion

--- Insert Track/Insert Track.lua
+++ Insert Track/Insert Track.lua
@@ -3,11 +3,7 @@ function Foreign_OnCommand(cmd_name)
    reaper.Main_OnCommand(cmd_id, 0)
 end
 
-reaper.Main_OnCommand(40001, 0) -- Track: Insert new track
-new_track = reaper.GetLastTouchedTrack()
-parent = reaper.GetParentTrack(new_track)
-
-if parent then
+function update_folder_color(new_track)   
    -- SWS: Select only parent of selected track
    Foreign_OnCommand("_SWS_SELPARENTS")
 
@@ -16,3 +12,20 @@ if parent then
 
    reaper.SetOnlyTrackSelected(new_track, false)
 end
+
+reaper.Undo_BeginBlock()
+
+nb_selected_tracks = reaper.CountSelectedTracks(0)
+if nb_selected_tracks > 0 then
+   last_track = reaper.GetSelectedTrack(0, nb_selected_tracks - 1)
+   reaper.Main_OnCommand(40001, 0) -- Track: Insert new track
+else
+   reaper.InsertTrackAtIndex(reaper.GetNumTracks(), false)
+end
+
+-- update folder colour if necessary
+new_track = reaper.GetLastTouchedTrack()
+new_track_has_parent = reaper.GetTrackDepth(new_track) > 0 -- reaper.GetParentTrack(new_track) and true
+if new_track_has_parent then update_folder_color(new_track) end
+
+reaper.Undo_EndBlock("Script: Track Inserted", 0)
