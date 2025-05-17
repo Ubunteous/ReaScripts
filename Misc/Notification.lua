5c51b5a929146615fe5cc45cd47d320d71a22cd5
Ubunteous <46612154+Ubunteous@users.noreply.github.com>
2025-05-17 20:54:22 +0200

    Finish notification

--- Misc/Notification.lua
+++ Misc/Notification.lua
@@ -24,43 +24,32 @@ function getClientHeight(hwnd)
    return right-left, bottom-top
 end
 
-local function Main()
+function ShowNotification(message)
    local char = gfx.getchar()
 
+   local height, width = getClientHeight(reaper.GetMainHwnd())
+   gfx.init(message, 400, 100, 0, width // 1.25, height // 5)
+
    local elapsed = reaper.time_precise() - time_start
-   -- if char ~= 27 and char ~= -1 then
    if elapsed < 1 then
-	  reaper.defer(Main)
+	  reaper.defer(ShowNotification)
    end
 
    gfx.setfont(1, "Arial", 44)
-   local my_str = "This is a string"
-   local x, y = 170, 20
+   local x, y = 180, 20
    local w, h = 50, 50
-   local r = 10
 
-   -- gfx.roundrect(x, y, w, h, r, 1, 0)
-
-   local str_w, str_h = gfx.measurestr(my_str)
-   local txt_x = x + ((w - str_w) / 2)
-   local txt_y = y + ((h - str_h) / 2)
+   local str_w, str_h = gfx.measurestr(message)
+   local txt_x = x + (w - str_w) / 2
+   local txt_y = y + (h - str_h) / 2
 
    gfx.x = txt_x
    gfx.y = txt_y
+   gfx.set(0.25, 0.5, 0.25, 1)
 
-   gfx.set(0.25, 0.5, 0.25, 1) -- text
-   gfx.drawstr(my_str)
-   -- gfx.set(0.25, 0, 0.25) -- bg
-
+   gfx.drawstr("My Window")
    gfx.update()
 end
 
-
-local height, width = getClientHeight(reaper.GetMainHwnd())
--- local mouse_x, mouse_y = reaper.GetMousePosition()
-
-gfx.init("My Window", 400, 100, 0, width // 1.25, height // 5)
 time_start = reaper.time_precise()
-Main()
-
--- reaper.JS_Mouse_SetPosition(mouse_x, mouse_y)
+ShowNotification("My Window")
